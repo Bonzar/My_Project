6d099e00f86dc5526cf077bab406736913eee560
Vladislav <nva112002@gmail.com>
2020-05-02 19:56:12 +0300

    Working v3.0.0 on PC

--- AWS/Warehouse/Warehouse.py
+++ AWS/Warehouse/Warehouse.py
@@ -2,20 +2,75 @@ import json
 import re
 
 
+def short_name(letter):
+    try:
+        dict_of_equipment = {'Printer': 'P', 'Scanner': 'S', 'PrinterScanner': 'PS', 'Xerox': 'X', 'P': 'Printer',
+                             'S': 'Scanner', 'Ps': 'PrinterScanner', 'X': 'Xerox'}
+        return dict_of_equipment[letter]
+    except KeyError:
+        print('Not equipment!')
+        return "Back"
+
+
+def dell(equipment):
+    try:
+        if len(Warehouse.list_equipment[equipment]):
+            print('Models on warehouse:')
+            dict_of_models_to_delete = {}
+            for i in range(len(Warehouse.list_equipment[equipment])):
+                print(f'{i + 1}) {Warehouse.list_equipment[equipment][i]["model"]}')
+                dict_of_models_to_delete[i + 1] = Warehouse.list_equipment[equipment][i]["model"]
+        else:
+            raise EmptyModel
+        new_model = input('Enter a model(or a number of model) to delete: ')
+        return dict_of_models_to_delete[int(new_model)] if new_model.isdigit() else new_model
+    except EmptyModel:
+        return
+
+
+class EmptyModel(Exception):
+    def __init__(self):
+        print('Nothing to delete!')
+
+
 class TypeOrgEquipmentError(Exception):
     def __init__(self, name, model):
         print(f'This model: {model} is not {name}')
 
 
-class Warehouse:
-    with open("/home/ubuntu/My_Project/AWS/Warehouse/WarehouseList.json", 'r', encoding='utf-8') as WL1:
-        list_equipment = json.load(WL1)
+def default():
+    with open("WarehouseList.json", 'w', encoding='utf-8') as WLw:
+        list_equipment_default = {'Printer': [], 'Scanner': [], 'PrinterScanner': [], 'Xerox': []}
+        json.dump(list_equipment_default, WLw, indent=4)
 
-    @staticmethod
-    def default():
-        with open("/home/ubuntu/My_Project/AWS/Warehouse/WarehouseList.json", 'w', encoding='utf-8') as WLw:
-            list_equipment_default = {'Printer': [], 'Scanner': [], 'PrinterScanner': [], 'Xerox': []}
-            json.dump(list_equipment_default, WLw, indent=4)
+
+class Warehouse:
+    with open("WarehouseList.json", 'r', encoding='utf-8') as WL1:
+        try:
+            list_equipment = json.load(WL1)
+        except json.decoder.JSONDecodeError:
+            default()
+            list_equipment = json.load(WL1)
+
+
+def add_to_warehouse(equipment, *args):
+    try:
+        count = int(input(f'How many {equipment} {args[0]["firm"]} {args[0]["model"]} to add: '))
+    except ValueError:
+        print('Not a number was entered')
+        return
+    with open("WarehouseList.json", 'w', encoding='utf-8') as WLw:
+        if Warehouse.list_equipment[equipment]:
+            for i in range(len(Warehouse.list_equipment[equipment])):
+                if Warehouse.list_equipment[equipment][i]["model"].upper() == args[0]["model"].upper():
+                    Warehouse.list_equipment[equipment][i]['count'] += count
+                    json.dump(Warehouse.list_equipment, WLw, indent=4)
+                    return
+        Warehouse.list_equipment[equipment].append({})
+        for el in args:
+            Warehouse.list_equipment[equipment][-1] = {par: value for par, value in el.items()}
+        Warehouse.list_equipment[equipment][-1]['count'] = count
+        json.dump(Warehouse.list_equipment, WLw, indent=4)
 
 
 class OrgEquipment:
@@ -26,47 +81,13 @@ class OrgEquipment:
         self._list_of_params = {'firm': firm, 'cost': cost, 'model': model}
 
     @classmethod
-    def add_to_warehouse(cls, *args):
-        try:
-            list_words = re.findall('[A-Z][^A-Z]*', cls.__name__)
-            if len(list_words) > 1:
-                first_letter = ''
-                for el in range(len(list_words)):
-                    first_letter += list_words[el][0]
-            else:
-                first_letter = cls.__name__.upper()[:2]
-            if args[0]["model"].upper().split('_')[0][:2] == first_letter:
-                pass
-            else:
-                raise TypeOrgEquipmentError(cls.__name__, args[0]["model"])
-        except TypeOrgEquipmentError:
-            return
-        try:
-            count = int(input(f'How many {cls.__name__} {args[0]["firm"]} {args[0]["model"]} to add: '))
-        except ValueError as VE:
-            print('Not a number was entered')
-            return
-        with open("/home/ubuntu/My_Project/AWS/Warehouse/WarehouseList.json", 'w', encoding='utf-8') as WLw:
-            if Warehouse.list_equipment[cls.__name__]:
-                for i in range(len(Warehouse.list_equipment[cls.__name__])):
-                    if Warehouse.list_equipment[cls.__name__][i]["model"].upper() == args[0]["model"].upper():
-                        Warehouse.list_equipment[cls.__name__][i]['count'] += count
-                        json.dump(Warehouse.list_equipment, WLw, indent=4)
-                        return
-            Warehouse.list_equipment[cls.__name__].append({})
-            for el in args:
-                Warehouse.list_equipment[cls.__name__][-1] = {par: value for par, value in el.items()}
-            Warehouse.list_equipment[cls.__name__][-1]['count'] = count
-            json.dump(Warehouse.list_equipment, WLw, indent=4)
-
-    @classmethod
     def del_from_warehouse(cls, *args):
         try:
             count = int(input(f'How many {cls.__name__} {args[0]["model"]} to delete: '))
         except ValueError as VE:
             print('Not a number was entered')
             return
-        with open('/home/ubuntu/My_Project/AWS/Warehouse/WarehouseList.json', 'w', encoding='utf-8') as WLw:
+        with open('WarehouseList.json', 'w', encoding='utf-8') as WLw:
             if Warehouse.list_equipment[cls.__name__]:
                 for i in range(len(Warehouse.list_equipment[cls.__name__])):
                     if Warehouse.list_equipment[cls.__name__][i]["model"].upper() == args[0]["model"].upper():
@@ -95,8 +116,8 @@ class Printer(OrgEquipment):
         elif self.model[-1].upper() == 'C':
             self.type_paint = 'Colorful'
 
-    def add_printer(self):
-        Printer.add_to_warehouse(self._list_of_params)
+    def add_printer(self, equipment):
+        add_to_warehouse(equipment, self._list_of_params)
 
     def del_printer(self):
         Printer.del_from_warehouse(self._list_of_params)
@@ -113,12 +134,12 @@ class Scanner(OrgEquipment):
         if self.version_scanner is not None:
             pass
         elif 'S' in self.model:
-            number_of_version = self.model[2]
+            number_of_version = self.model[1]
             if number_of_version.isdigit() and 1 <= int(number_of_version) <= 3:
                 self.version_scanner = int(number_of_version)
 
-    def add_scanner(self):
-        Scanner.add_to_warehouse(self._list_of_params)
+    def add_scanner(self, equipment):
+        add_to_warehouse(equipment, self._list_of_params)
 
     def del_scanner(self):
         Scanner.del_from_warehouse(self._list_of_params)
@@ -134,16 +155,16 @@ class PrinterScanner(Printer, Scanner):
         self._list_of_params = {'firm': firm, 'cost': cost, 'model': model, 'type_paint': self.type_paint,
                                 'version_scanner': self.version_scanner}
 
-    def add_printer_scanner(self):
-        PrinterScanner.add_to_warehouse(self._list_of_params)
+    def add_printer_scanner(self, equipment):
+        add_to_warehouse(equipment, self._list_of_params)
 
     def del_printer_scanner(self):
         PrinterScanner.del_from_warehouse(self._list_of_params)
 
 
 class Xerox(OrgEquipment):
-    def add_xerox(self):
-        Xerox.add_to_warehouse(self._list_of_params)
+    def add_xerox(self, equipment):
+        add_to_warehouse(equipment, self._list_of_params)
 
     def del_xerox(self):
         Xerox.del_from_warehouse(self._list_of_params)
--- AWS/Warehouse/WarehousePlay.py
+++ AWS/Warehouse/WarehousePlay.py
@@ -5,101 +5,83 @@ from Warehouse import *
 """   Welcome to the office equipment accounting program.
     ------------------------------------------------------
     We have 4 type's of equipment:
-        -Printer(Pr)
-        -Scanner(Sc)
+        -Printer(P)
+        -Scanner(S)
         -PrinterScanner(PS)
-        -Xerox(Xe)
+        -Xerox(X)
     You can add and del certain types of office equipment
     with a specific firm model and properties different
     for each type of equipment.
     ------------------------------------------------------
     Specification for models:
-        First 2 symbols - abbreviation of Name office equipment
+        First symbol(Two for PS) - abbreviation of Name office equipment
         For Scanner - after the abbreviation should be number - version of scanner
         For Printer - in the end of model should be a letter (b/c) that mean type of painting (Black&White / RGB)
         For PrinterScanner both of the top.
 """
-
 while True:
     command = input('\n''Enter a command: ').title()
-    if command == 'Help':
-        print('Available command:\n'
-              '-Help (list of available commands)\n'
-              '-Add (add equipment to warehouse)\n'
-              '-Del (del equipment from warehouse)\n'
-              '-List (display the contents of the warehouse)\n'
-              '-Default (reset the contents of the warehouse)')
-    elif command == 'Add':
+    if command == 'Add' or command == '1':
         equipment = input('Choice equipment:\n-Printer(P)\n-Scanner(S)\n-PrinterScanner(PS)\n-Xerox(X)\n:').title()
+        if len(equipment) <= 2:
+            equipment = short_name(equipment)
+        else:
+            equipment = short_name(short_name(equipment))
         if equipment == 'Back':
             continue
         firm = input('Firm: ')
-        model = input('Model: ')
-        cost = int(input('Cost: '))
-        if equipment == 'Printer' or equipment == 'P':
+        model = short_name(equipment) + input(f'Model: {short_name(equipment)}')
+        cost = input('Cost: ')
+        while not cost.isdigit():
+            cost = input(': ')
+        if equipment == 'Printer':
             p = Printer(model, firm, cost)
-            Printer.add_printer(p)
-        elif equipment == 'Scanner' or equipment == 'S':
+            Printer.add_printer(p, equipment=equipment)
+        elif equipment == 'Scanner':
             s = Scanner(model, firm, cost)
-            Scanner.add_scanner(s)
-        elif equipment == 'Printerscanner' or equipment == 'Ps':
+            Scanner.add_scanner(s, equipment=equipment)
+        elif equipment == 'PrinterScanner':
             ps = PrinterScanner(model, firm, cost)
-            PrinterScanner.add_printer_scanner(ps)
-        elif equipment == 'Xerox' or equipment == 'X':
+            PrinterScanner.add_printer_scanner(ps, equipment=equipment)
+        elif equipment == 'Xerox':
             x = Xerox(model, firm, cost)
-            Xerox.add_xerox(x)
-    elif command == 'Del':
+            Xerox.add_xerox(x, equipment=equipment)
+    elif command == 'Del' or command == '2':
         equipment = input('Choice equipment:\n-Printer(P)\n-Scanner(S)\n-PrinterScanner(PS)\n-Xerox(X)\n:').title()
+        if len(equipment) <= 2:
+            equipment = short_name(equipment)
+        else:
+            equipment = short_name(short_name(equipment))
         if equipment == 'Back':
             continue
-        if equipment == 'Printer' or equipment == 'P':
-            print('Models on warehouse:')
-            if len(Warehouse.list_equipment['Printer']):
-                for i in range(len(Warehouse.list_equipment['Printer'])):
-                    print(Warehouse.list_equipment["Printer"][i]["model"])
-            else:
-                print('Nothing to delete!')
-                continue
-            model = input('Enter model: ')
+        model = dell(equipment)
+        if model is None:
+            continue
+        if equipment == 'Printer':
             p = Printer(model)
             Printer.del_printer(p)
-        elif equipment == 'Scanner' or equipment == 'S':
-            print('Models on warehouse:')
-            if len(Warehouse.list_equipment['Scanner']):
-                for i in range(len(Warehouse.list_equipment['Scanner'])):
-                    print(Warehouse.list_equipment["Scanner"][i]["model"])
-            else:
-                print('Nothing to delete!')
-                continue
-            model = input('Enter model: ')
+        elif equipment == 'Scanner':
             s = Scanner(model)
             Scanner.del_scanner(s)
-        elif equipment == 'Printerscanner' or equipment == 'Ps':
-            print('Models on warehouse:')
-            if len(Warehouse.list_equipment['PrinterScanner']):
-                for i in range(len(Warehouse.list_equipment['PrinterScanner'])):
-                    print(Warehouse.list_equipment["PrinterScanner"][i]["model"])
-            else:
-                print('Nothing to delete!')
-                continue
-            model = input('Enter model: ')
+        elif equipment == 'PrinterScanner':
             p = PrinterScanner(model)
             PrinterScanner.del_printer_scanner(p)
-        elif equipment == 'Xerox' or equipment == 'X':
-            print('Models on warehouse:')
-            if len(Warehouse.list_equipment['Xerox']):
-                for i in range(len(Warehouse.list_equipment['Xerox'])):
-                    print(1,'', Warehouse.list_equipment["Xerox"][i]["model"])
-            else:
-                print('Nothing to delete!')
-                continue
-            model = input('Enter model: ')
+        elif equipment == 'Xerox':
             x = Xerox(model)
             Xerox.del_xerox(x)
-    elif command == 'Default':
-        Warehouse.default()
-    elif command == 'Exit':
-        raise SystemExit
-    elif command == 'List':
-        with open('/home/ubuntu/My_Project/AWS/Warehouse/WarehouseList.json', 'r', encoding='utf-8') as List:
+    elif command == 'List' or command == '3':
+        with open('WarehouseList.json', 'r', encoding='utf-8') as List:
             print(List.read())
+    elif command == 'Default' or command == '4':
+        default()
+    elif command == 'Exit' or command == '5':
+        raise SystemExit
+
+    elif command == 'Help'  or command == '6':
+        print('Available command:\n'
+              '1) Add (add equipment to warehouse)\n'
+              '2) Del (del equipment from warehouse)\n'
+              '3) List (display the contents of the warehouse)\n'
+              '4) Default (reset the contents of the warehouse)\n'
+              '5) Exit (out of program)\n'
+              '6) Help (list of available commands)')
